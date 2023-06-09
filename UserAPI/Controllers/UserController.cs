30aefc1d03f571cb247c376b38b633dac99ba8cd
GlenCosby <cosby-g1@ulster.ac.uk>
2023-06-09 06:49:15 +0100

    App upload

---
+++ UserAPI/Controllers/UserController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using UserAPI.Models;
+
+namespace UserAPI.Controllers
+{
+    [Route("users/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DatabaseContext dbContext;
+
+        public UserController(DatabaseContext context)
+        {
+            dbContext = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> Get()
+        {
+            if (dbContext.Users.IsNullOrEmpty())
+                return BadRequest("User list is empty.");
+
+            return Ok(await dbContext.Users.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<User>>> Get(int id)
+        {
+            var user = await dbContext.Users.FindAsync(id);
+            if (user == null)
+                return BadRequest("User not found.");
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<User>>> Post(User user)
+        {
+            dbContext.Users.Add(user);
+            await dbContext.SaveChangesAsync();
+
+            return Ok(await dbContext.Users.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<User>>> Put(User updated)
+        {
+            var dbUser = await dbContext.Users.FindAsync(updated.id);
+            if (dbUser == null)
+                return BadRequest("User not found.");
+
+            dbUser.username = updated.username;
+
+            await dbContext .SaveChangesAsync();
+
+            return Ok(await dbContext.Users.ToListAsync());
+        }
+
+        [HttpDelete ("{id}")]
+        public async Task<ActionResult<List<User>>> Delete(int id)
+        {
+            var dbUser = await dbContext.Users.FindAsync(id);
+            if (dbUser == null)
+                return BadRequest("User not found.");
+
+            dbContext.Users.Remove(dbUser);
+            await dbContext.SaveChangesAsync();
+
+            return Ok(await dbContext.Users.ToListAsync());
+        }
+
+
+    }
+}
---
+++ UserAPI/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UserAPI.Models
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int id { get; set; }
+
+        [Key]
+        public string username { get; set; } = string.Empty;
+    }
+}
